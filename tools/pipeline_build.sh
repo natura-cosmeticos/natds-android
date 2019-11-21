b2a68e71f6c5d051c785999dd9ed57c93073e603
Selem Afonso <selem32@gmail.com>
2019-11-21 15:14:03 -0300

    [DSY-263] - Config Travis Pipeline

--- tools/pipeline_build.sh
+++ tools/pipeline_build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 if [ $TRAVIS_BRANCH = "master" -a $TRAVIS_PULL_REQUEST = "false" ]; then
-    ./gradlew clean build publish -PdisablePreDex --stacktrace
+    ./gradlew clean build -PdisablePreDex --stacktrace
+#    ./gradlew clean build publish -PdisablePreDex --stacktrace
 else
     ./gradlew clean build -PdisablePreDex --stacktrace
 fi
