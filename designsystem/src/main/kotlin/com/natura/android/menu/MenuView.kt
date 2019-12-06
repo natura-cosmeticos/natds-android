18ccbdcf51d0df6c88c23191b873369c0dc15856
Angelica Oliveira <angelica.liv@gmail.com>
2019-12-06 10:50:52 -0300

    removing androidx

--- designsystem/src/main/kotlin/com/natura/android/menu/MenuView.kt
+++ designsystem/src/main/kotlin/com/natura/android/menu/MenuView.kt
@@ -3,13 +3,13 @@ package com.natura.android.menu
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.PorterDuff
+import android.support.constraint.ConstraintLayout
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.AppCompatImageView
+import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.appcompat.widget.AppCompatTextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
 import com.natura.android.R
 import com.natura.android.ext.setVisibilityFromBoolean
 
--- sample/src/main/kotlin/com/natura/android/sample/MainActivity.kt
+++ sample/src/main/kotlin/com/natura/android/sample/MainActivity.kt
@@ -2,16 +2,17 @@ package com.natura.android.sample
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 import com.natura.android.sample.components.ExpandableNavigationViewActivity
 import com.natura.android.sample.components.MenuActivity
 import com.natura.android.sample.components.SubmenuActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btn_submenu).setOnClickListener {
             startActivity(Intent(this, SubmenuActivity::class.java))
