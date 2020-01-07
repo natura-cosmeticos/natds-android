1fa24731506d80ab4a8280571e12373a1f25b2df
Selem Afonso <selem32@gmail.com>
2020-01-07 15:07:22 -0300

    Button Primary, Loading, Error Screen and News Icons Fonts

---
+++ sample/src/main/kotlin/com/natura/android/sample/components/ButtonPrimaryActivity.kt
@@ -0,0 +1,13 @@
+package com.natura.android.sample.components
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.natura.android.sample.R
+
+class ButtonPrimaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button_primary)
+    }
+}
---
+++ sample/src/main/kotlin/com/natura/android/sample/components/ErrorActivity.kt
@@ -0,0 +1,24 @@
+package com.natura.android.sample.components
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import com.natura.android.error.DsErrorDefault
+import com.natura.android.sample.R
+
+
+class ErrorActivity : AppCompatActivity() {
+
+    private val errorButton by lazy { findViewById<DsErrorDefault>(R.id.error_default) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_error)
+
+        errorButton.listener = {
+            val snackbar = Snackbar
+                .make(errorButton, "RECARREGANDO", Snackbar.LENGTH_LONG)
+            snackbar.show()
+        }
+    }
+}
