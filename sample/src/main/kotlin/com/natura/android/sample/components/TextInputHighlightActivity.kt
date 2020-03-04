70270a2f79981f77a2db212e733f27600330d4aa
Rodrigo Nunes <rodrigo.nunes.trd@c6bank.com>
2020-03-03 21:56:43 -0300

    add custom attributes

--- sample/src/main/kotlin/com/natura/android/sample/components/TextInputHighlightActivity.kt
+++ sample/src/main/kotlin/com/natura/android/sample/components/TextInputHighlightActivity.kt
@@ -3,10 +3,15 @@ package com.natura.android.sample.components
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.natura.android.sample.R
+import com.natura.android.widget.TextInputHighlight
 
 class TextInputHighlightActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_input_highlight)
+
+        val inputHighlight = findViewById<TextInputHighlight>(R.id.second_text_input_highlight)
+        inputHighlight.setDescription("Add description via code")
+        inputHighlight.setHighlightedInfo("$ 00.00")
     }
 }
