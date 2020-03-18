398ec9b533ff8e31ab644c4fd26e3e125a398a37
Patricia Muniz <pmuniz@thoughtworks.com>
2020-03-18 16:16:59 -0300

    Remove unused imports

--- sample/src/main/kotlin/com/natura/android/sample/components/DrawableAdapter.kt
+++ sample/src/main/kotlin/com/natura/android/sample/components/DrawableAdapter.kt
@@ -1,7 +1,6 @@
 package com.natura.android.sample.components
 
 import android.content.Context
-import android.support.v7.widget.AppCompatTextView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.natura.android.sample.R
-import java.util.*
 
 class DrawableAdapter(
     private val context: Context,
