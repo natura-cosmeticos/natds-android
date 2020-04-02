f8b26c02763e0614c46948392beef16aaa794bd3
Marcella Souza <mlsouza@thoughtworks.com>
2020-04-02 16:22:40 -0300

    [DSY-000] Adding tests steps to pipeline

--- tools/pipeline_build.sh
+++ tools/pipeline_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # task :sample:verifyDebugAndroidTestScreenshotTest removed because of an instability in Travis emulator
 if [ $TRAVIS_BRANCH = "master" -a $TRAVIS_PULL_REQUEST = "false" ]; then
-    ./gradlew clean ktlintFormat ktlint build publish -PdisablePreDex --stacktrace
+    ./gradlew clean build publish -PdisablePreDex --stacktrace
 else
-    ./gradlew clean ktlintFormat ktlint build -PdisablePreDex --stacktrace
+    ./gradlew clean build -PdisablePreDex --stacktrace
 fi
 
