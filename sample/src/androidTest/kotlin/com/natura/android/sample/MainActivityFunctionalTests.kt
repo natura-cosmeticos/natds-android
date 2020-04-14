16e16ecb168f7135538501cf88d4f7246c5829ea
Marcella Souza <mlsouza@thoughtworks.com>
2020-04-14 15:52:52 -0300

    [DSY-000] Adjusting test to lint rules

--- sample/src/androidTest/kotlin/com/natura/android/sample/MainActivityFunctionalTests.kt
+++ sample/src/androidTest/kotlin/com/natura/android/sample/MainActivityFunctionalTests.kt
@@ -1,6 +1,5 @@
 package com.natura.android.sample
 
-import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -8,8 +7,6 @@ import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
