d4758361cfa990ae55981c19eb4a2564f913d85b
Marcella Souza <mlsouza@thoughtworks.com>
2020-05-04 16:03:32 -0300

    [DSY-292] Adjusting dark theme button behavior

--- sample/src/main/kotlin/com/natura/android/sample/components/ColorsActivity.kt
+++ sample/src/main/kotlin/com/natura/android/sample/components/ColorsActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayout
 import com.natura.android.sample.R
@@ -34,17 +35,27 @@ class ColorsActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.colors_toolbar_menu, menu)
+        setDarkModeButtonIcon(darkMode, menu?.findItem(R.id.darkThemeButton))
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if (item?.itemId == R.id.darkThemeButton) {
+            setDarkModeButtonIcon(darkMode, item)
             darkMode = !darkMode
             restartScreenToApplyTheme(darkMode, currentTab)
         } else {
             onBackPressed()
         }
         return true
+    }
+
+    private fun setDarkModeButtonIcon(darkMode: Boolean, menuItem: MenuItem?) {
+        if (darkMode) {
+            menuItem?.setIcon(R.drawable.ds_ic_outlined_action_lightoff)
+        } else {
+            menuItem?.setIcon(R.drawable.ds_ic_outlined_action_lighton)
+        }
     }
 
     private fun colorToolBar(darkMode: Boolean) {
