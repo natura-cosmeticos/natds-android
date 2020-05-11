c6d56c2cfcdb93e33cdb1adef229e8442b1b8e88
Marcella Souza <mlsouza@thoughtworks.com>
2020-05-11 15:42:28 -0300

    [DSY-705] Adding unit test to elevation applied to screen

--- sample/src/test/kotlin/com/natura/android/sample/tokens/ElevationActivityTest.kt
+++ sample/src/test/kotlin/com/natura/android/sample/tokens/ElevationActivityTest.kt
@@ -20,9 +20,117 @@ class ElevationActivityTest {
     @Test
     fun checksElevationNoneSize() {
         elevationActivityScenario.onActivity { elevationActivity ->
-            val elevation = elevationActivity.cardElevationNone.elevation
+            val elevation = elevationActivity.cardElevationNone.cardElevation.toInt()
 
-            assertEquals(0.0, elevation)
+            assertEquals(0, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation1Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation1.cardElevation.toInt()
+
+            assertEquals(1, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation2Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation2.cardElevation.toInt()
+
+            assertEquals(2, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation3Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation3.cardElevation.toInt()
+
+            assertEquals(3, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation4Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation4.cardElevation.toInt()
+
+            assertEquals(4, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation5Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation5.cardElevation.toInt()
+
+            assertEquals(5, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation6Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation6.cardElevation.toInt()
+
+            assertEquals(6, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation7Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation7.cardElevation.toInt()
+
+            assertEquals(7, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation8Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation8.cardElevation.toInt()
+
+            assertEquals(8, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation9Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation9.cardElevation.toInt()
+
+            assertEquals(9, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation12Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation12.cardElevation.toInt()
+
+            assertEquals(12, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation16Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation16.cardElevation.toInt()
+
+            assertEquals(16, elevation)
+        }
+    }
+
+    @Test
+    fun checksElevation24Size() {
+        elevationActivityScenario.onActivity { elevationActivity ->
+            val elevation = elevationActivity.cardElevation24.cardElevation.toInt()
+
+            assertEquals(24, elevation)
         }
     }
 }
