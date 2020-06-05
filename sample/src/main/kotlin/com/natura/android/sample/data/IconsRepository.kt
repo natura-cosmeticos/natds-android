5d6f9db9ba268dd9821b414fc7b6581079736dc4
Marcella Souza <mlsouza@thoughtworks.com>
2020-06-05 20:08:26 -0300

    [DSY-755] Adjusting load icons name logic

--- sample/src/main/kotlin/com/natura/android/sample/data/IconsRepository.kt
+++ sample/src/main/kotlin/com/natura/android/sample/data/IconsRepository.kt
@@ -2,6 +2,8 @@ package com.natura.android.sample.data
 
 import android.content.Context
 import android.content.res.Resources
+import com.natura.android.sample.R
+import java.io.BufferedReader
 import java.io.File
 
 class IconsRepository (val context: Context) {
@@ -9,14 +11,23 @@ class IconsRepository (val context: Context) {
     fun getIconsIdsList(): List<Int> {
         val rawIconList = getIconsNamesFromFile()
         val iconsIdsList = mutableListOf<Int>()
+        val id = R.drawable.filled_action_add
         for (iconName in rawIconList) {
-            val iconName = "R.id.$iconName"
             iconsIdsList.add(context.resources.getIdentifier(iconName, "drawable", context.packageName))
         }
         return iconsIdsList
     }
 
     fun getIconsNamesFromFile(): List<String> {
-        return File("icons-map.json").useLines { it.toList() }
+        val reader = BufferedReader(context.assets.open("icons_map.txt").bufferedReader())
+        val iconsList = mutableListOf<String>()
+        reader.use { reader ->
+            var line = reader.readLine()
+            while (line != null) {
+                iconsList.add(line)
+                line = reader.readLine()
+            }
+        }
+        return iconsList
     }
 }
