a7fc223786a4a990a8d731eb72239ca50aa946dc
Marcella Souza <mlsouza@thoughtworks.com>
2020-07-10 17:57:49 -0300

    style: 💄 [DSY-1103] fix lint issues

--- sample/src/androidTest/kotlin/com/natura/android/sample/MainActivityFunctionalTests.kt
+++ sample/src/androidTest/kotlin/com/natura/android/sample/MainActivityFunctionalTests.kt
@@ -1,17 +1,13 @@
 package com.natura.android.sample
 
-import android.widget.TextView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withParent
-import androidx.test.espresso.matcher.ViewMatchers.withResourceName
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.CoreMatchers.*
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.Before
