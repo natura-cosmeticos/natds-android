013961404c7cc59efb95f61c03b323b2d3bcd212
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-07-13 15:43:04 -0300

    Add isExpanded example to sample

--- sample/src/main/kotlin/com/natura/android/sample/components/ExpansionPanelActivity.kt
+++ sample/src/main/kotlin/com/natura/android/sample/components/ExpansionPanelActivity.kt
@@ -2,6 +2,7 @@ package com.natura.android.sample.components
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.natura.android.expansionPanel.ExpansionPanel
@@ -20,16 +21,19 @@ class ExpansionPanelActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Expansion Panel"
 
-        setupExpansionPanelFeatures()
-    }
-
-    private fun setupExpansionPanelFeatures() {
         val expansionPanel = findViewById<ExpansionPanel>(R.id.expansion_panel)
+        val currentStateButton = findViewById<Button>(R.id.current_state_button)
 
         expansionPanel.setOnStateChangeListener { isExpanded ->
             val action = if (isExpanded) "expanded" else "collapsed"
 
             Toast.makeText(this, "The panel $action.", Toast.LENGTH_SHORT).show()
+        }
+
+        currentStateButton.setOnClickListener {
+            val state = if (expansionPanel.isExpanded()) "expanded" else "collapsed"
+
+            Toast.makeText(this, "The panel is $state.", Toast.LENGTH_SHORT).show()
         }
     }
 
