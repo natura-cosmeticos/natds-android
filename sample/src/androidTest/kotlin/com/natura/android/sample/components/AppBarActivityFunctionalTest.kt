4e720ebfa491469ecab099d44c60cbcd941dd1f3
Anderson Carvalho <acarvalh@thoughtworks.com>
2020-07-17 16:51:16 -0300

    DSY-1202 fix tests

--- sample/src/androidTest/kotlin/com/natura/android/sample/components/AppBarActivityFunctionalTest.kt
+++ sample/src/androidTest/kotlin/com/natura/android/sample/components/AppBarActivityFunctionalTest.kt
@@ -3,15 +3,11 @@ package com.natura.android.sample.components
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.natura.android.sample.R
-import org.hamcrest.Matchers.not
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -38,6 +34,6 @@ class AppBarActivityFunctionalTest {
     fun showSearchBar() {
         onView(withId(R.id.searchMenuBtn)).perform(click())
 
-        onView(ViewMatchers.withText("Search...")).check(matches(isDisplayed()))
+        onView(withId(R.id.appBar)).check(matches(isDisplayed()))
     }
 }
