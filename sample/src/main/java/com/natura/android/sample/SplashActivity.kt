704d38a23fd6e33d31189fe91efa3f4a0877f9ba
Marcella Souza <mlsouza@thoughtworks.com>
2020-09-01 09:27:52 -0300

    feat: 🎸 [DSY-1423] Adjusting full screen mode

--- sample/src/main/java/com/natura/android/sample/SplashActivity.kt
+++ sample/src/main/java/com/natura/android/sample/SplashActivity.kt
@@ -1,11 +1,41 @@
 package com.natura.android.sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 
 class SplashActivity : AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 2000
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, BrandSelectorActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+//        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        mDelayHandler = Handler()
+
+        mDelayHandler?.postDelayed(mRunnable, SPLASH_DELAY)
+
     }
+
+    public override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
 }
