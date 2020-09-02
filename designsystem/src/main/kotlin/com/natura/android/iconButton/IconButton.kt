ac44a82aaccf957cae21104e3de9ac6861e14a36
Guilherme Leonel <gleonel@thoughtworks.com>
2020-09-02 13:55:14 -0300

    feat: 🎸 [DSY-1332] - Add ripple effect, color, size and enable properties of IconButton component

--- designsystem/src/main/kotlin/com/natura/android/iconButton/IconButton.kt
+++ designsystem/src/main/kotlin/com/natura/android/iconButton/IconButton.kt
@@ -8,26 +8,33 @@ import android.util.TypedValue
 import android.view.View
 import android.widget.ImageButton
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.getBooleanOrThrow
+import androidx.core.content.res.getIntOrThrow
 import androidx.core.content.res.getResourceIdOrThrow
 import com.natura.android.R
 import com.natura.android.exceptions.MissingThemeException
 
 class IconButton @JvmOverloads constructor(
-
     context: Context,
     private val attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     private var iconButtonAttributesArray: TypedArray
-    private var sizeResourceAttribute = 0
     private var iconColorResourceAttribute = 0
     private var iconAttribute: Int? = null
     private var sizeAttribute: Int? = null
     private var colorAttribute: Int? = null
-    private var notifyAttribute: Int? = null
 
     private val iconButton by lazy { findViewById<ImageButton>(R.id.iconButton) }
+    private val iconButtonContainer by lazy { findViewById<ConstraintLayout>(R.id.iconButtonMainContainer) }
+
+    override fun setEnabled(enabled: Boolean) {
+        iconButton.isEnabled = enabled
+
+        super.setEnabled(enabled)
+    }
 
     init {
         try {
@@ -36,18 +43,19 @@ class IconButton @JvmOverloads constructor(
             throw (MissingThemeException())
         }
 
-        iconButtonAttributesArray = context.obtainStyledAttributes(attrs, R.styleable.ds_icon_button)
+        iconButtonAttributesArray = context.obtainStyledAttributes(attrs, R.styleable.IconButton)
 
         getIconButtonAttributes()
         getAttributesFromTheme()
-        configureIconButton()
+        configureSize(sizeAttribute)
+        configureColor(colorAttribute)
 
         iconButtonAttributesArray.recycle()
     }
 
     fun setIcon(icon: Int?) {
         icon?.apply {
-            iconButton.setBackgroundResource(icon)
+            iconButton.setImageResource(icon)
         }
     }
 
@@ -55,120 +63,108 @@ class IconButton @JvmOverloads constructor(
         return iconButton.background
     }
 
+    private fun getIconButtonAttributes() {
+        getIconAttribute()
+        getSizeAttribute()
+        getColorAttribute()
+        getEnabledAttribute()
+    }
+
+    private fun getIconAttribute() {
+        try {
+            iconAttribute = iconButtonAttributesArray.getResourceIdOrThrow(R.styleable.IconButton_buttonIcon)
+        } catch (e: Exception) {
+            throw (IllegalArgumentException("⚠️ ⚠️ Missing iconButton required argument. You MUST set the iconButton icon(drawable).", e))
+        }
+    }
+
+    private fun getSizeAttribute() {
+        try {
+            sizeAttribute = iconButtonAttributesArray.getIntOrThrow(R.styleable.IconButton_size)
+        } catch (e: Exception) {
+            throw (IllegalArgumentException("⚠️ ⚠️ Missing iconButton required argument. You MUST set the iconButton size.", e))
+        }
+    }
+
+    private fun getColorAttribute() {
+        try {
+            colorAttribute = iconButtonAttributesArray.getIntOrThrow(R.styleable.IconButton_buttonColor)
+        } catch (e: Exception) {
+            throw (IllegalArgumentException("⚠️ ⚠️ Missing iconButton required argument. You MUST set the iconButton color.", e))
+        }
+    }
+
+    private fun getEnabledAttribute() {
+        try {
+            isEnabled = iconButtonAttributesArray.getBooleanOrThrow(R.styleable.IconButton_android_enabled)
+        } catch (e: Exception) {
+            throw (IllegalArgumentException("⚠️ ⚠️ Missing iconButton required argument. You MUST set the iconButton enable.", e))
+        }
+    }
+
     private fun getAttributesFromTheme() {
         try {
-            if (sizeAttribute == SMALL) {
-                setSizeAttributes(R.attr.iconButtonSmall)
-            } else {
-                setSizeAttributes(R.attr.iconButtonMedium)
+            when (colorAttribute) {
+                PRIMARY -> setColorAttribute(R.attr.iconButtonPrimary)
+                DEFAULT -> setColorAttribute(R.attr.iconButtonDefault)
             }
 
-            if (colorAttribute == PRIMARY) {
-                setColorAttributes(R.attr.iconButtonPrimary)
-            } else {
-                setColorAttributes(R.attr.iconButtonDefault)
+            if (!isEnabled) {
+                setColorAttribute(R.attr.iconButtonDisabled)
             }
         } catch (e: Exception) {
             throw (MissingThemeException())
         }
     }
 
-    private fun setSizeAttributes(attribute: Int) {
+    private fun setColorAttribute(attribute: Int) {
         context
             .theme
             .obtainStyledAttributes(
                 attrs,
-                R.styleable.ds_icon_button,
+                R.styleable.IconButton,
                 attribute,
                 0
             )
             .apply {
-                sizeResourceAttribute = this.getResourceIdOrThrow(R.styleable.ds_icon_button_icon_button_padding)
+                iconColorResourceAttribute = this.getResourceIdOrThrow(R.styleable.IconButton_iconColor)
             }
     }
 
-    private fun setColorAttributes(attribute: Int) {
-        context
-            .theme
-            .obtainStyledAttributes(
-                attrs,
-                R.styleable.ds_icon_button,
-                attribute,
-                0
-            )
-            .apply {
-                iconColorResourceAttribute = this.getResourceIdOrThrow(R.styleable.ds_icon_button_icon_button_iconColor)
+    private fun configureSize(size: Int?) {
+        size?.apply {
+            when (this) {
+                SMALL -> setSmallSize()
+                MEDIUM -> setMediumSize()
             }
-    }
-
-    private fun getIconButtonAttributes() {
-        getIconAttribute()
-        getSizeAttribute()
-        getNotifyAttribute()
-        getColorAttribute()
-    }
-
-    private fun getIconAttribute() {
-        try {
-            iconAttribute = iconButtonAttributesArray.getResourceIdOrThrow(R.styleable.ds_icon_button_icon_button_icon)
-        } catch (e: Exception) {
-            throw (IllegalArgumentException("⚠️ ⚠️ Missing shortcut required argument. You MUST set the iconButton icon(drawable).", e))
         }
     }
 
-    private fun getSizeAttribute() {
-        try {
-            sizeAttribute = iconButtonAttributesArray.getResourceIdOrThrow(R.styleable.ds_icon_button_icon_button_size)
-        } catch (e: Exception) {
-            throw (IllegalArgumentException("⚠️ ⚠️ Missing shortcut required argument. You MUST set the iconButton size.", e))
-        }
-    }
+    private fun configureColor(color: Int?) {
 
-    private fun getNotifyAttribute() {
-        try {
-            notifyAttribute = iconButtonAttributesArray.getResourceIdOrThrow(R.styleable.ds_icon_button_icon_button_notify)
-        } catch (e: Exception) {
-            throw (IllegalArgumentException("⚠️ ⚠️ Missing shortcut required argument. You MUST set the nofify attribute", e))
-        }
-    }
-
-    private fun getColorAttribute() {
-        try {
-            colorAttribute = iconButtonAttributesArray.getResourceIdOrThrow(R.styleable.ds_icon_button_icon_button_color)
-        } catch (e: Exception) {
-            throw (IllegalArgumentException("⚠️ ⚠️ Missing shortcut required argument. You MUST set the iconButton color.", e))
-        }
-    }
-
-    private fun configureIconButton() {
         setIcon(iconAttribute)
-        setMediumSize()
+        iconButton.setColorFilter(ContextCompat.getColor(context, iconColorResourceAttribute), android.graphics.PorterDuff.Mode.SRC_IN)
+
+        color?.apply {
+            when (this) {
+                PRIMARY -> iconButton.background = resources.getDrawable(R.drawable.iconbutton_ripple_background_primary, context.theme)
+                DEFAULT -> iconButton.background = resources.getDrawable(R.drawable.iconbutton_ripple_background_default, context.theme)
+            }
+        }
     }
 
     private fun setSmallSize() {
-        var parameter = iconButton.layoutParams as LayoutParams
+        var containerParameter = iconButtonContainer.layoutParams as LayoutParams
 
-//        parameter.width = getDimenFromTheme(R.attr.sizeSemi)
-//        parameter.height = getDimenFromTheme(R.attr.sizeSemi)
-
-        parameter.setMargins(
-            getDimenFromTheme(R.attr.spacingMicro),
-            getDimenFromTheme(R.attr.spacingMicro),
-            getDimenFromTheme(R.attr.spacingMicro),
-            getDimenFromTheme(R.attr.spacingMicro))
+        containerParameter.width = getDimenFromTheme(R.attr.sizeSemi)
+        containerParameter.height = getDimenFromTheme(R.attr.sizeSemi)
     }
 
     private fun setMediumSize() {
-        var parameter = iconButton.layoutParams as LayoutParams
+        var containerParameter = iconButtonContainer.layoutParams as LayoutParams
 
-//        parameter.width = getDimenFromTheme(R.attr.sizeSemiX)
-//        parameter.height = getDimenFromTheme(R.attr.sizeSemiX)
-
-        parameter.setMargins(
-            getDimenFromTheme(R.attr.spacingTiny),
-            getDimenFromTheme(R.attr.spacingTiny),
-            getDimenFromTheme(R.attr.spacingTiny),
-            getDimenFromTheme(R.attr.spacingTiny))
+        containerParameter.width = getDimenFromTheme(R.attr.sizeSemiX)
+        containerParameter.height = getDimenFromTheme(R.attr.sizeSemiX)
     }
 
     private fun getDimenFromTheme(attributeName: Int): Int {
--- sample/src/main/kotlin/com/natura/android/sample/components/IconButtonActivity.kt
+++ sample/src/main/kotlin/com/natura/android/sample/components/IconButtonActivity.kt
@@ -3,10 +3,13 @@ package com.natura.android.sample.components
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.natura.android.iconButton.IconButton
 import com.natura.android.sample.R
 import com.natura.android.sample.setChosenDefaultTheme
+import kotlinx.android.synthetic.main.activity_icon_button.*
 
 class IconButtonActivity : AppCompatActivity() {
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setChosenDefaultTheme()
@@ -16,10 +19,26 @@ class IconButtonActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Icon Button"
+        setClickListener()
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         onBackPressed()
         return true
+    }
+
+    private fun setClickListener() {
+        for (i in 0 until iconButtonContainer.childCount) {
+            if (iconButtonContainer.getChildAt(i) is IconButton) {
+                iconButtonContainer.getChildAt(i).setOnClickListener {
+                    increaseCount()
+                }
+            }
+        }
+    }
+
+    private fun increaseCount() {
+        count += 1
+        clickCounter.text = "Counter click $count"
     }
 }
