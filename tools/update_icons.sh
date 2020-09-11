d27d7aeafdc9d11891d1a123f5030dd442083bbf
Guilherme Leonel <gleonel@thoughtworks.com>
2020-09-11 13:51:31 -0300

    feat: 🎸 [DSY-1334] - Add and update icons

--- tools/update_icons.sh
+++ tools/update_icons.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd designsystem/src/main/res/drawable
+rm -rf ../../../../../sample/src/main/assets/icons_map.txt
 (ls -d outlined_* && ls -d filled_*) >> ../../../../../sample/src/main/assets/icons_map.txt
 sed -i -e 's/.xml//g' ../../../../../sample/src/main/assets/icons_map.txt
-rm -rf icons_map.txt-e
+rm -rf ../../../../../sample/src/main/assets/icons_map.txt-e
