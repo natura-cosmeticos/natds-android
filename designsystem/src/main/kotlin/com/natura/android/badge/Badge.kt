31b8910e2fc8d669909f438bb901a48e47c2a218
Marcella Souza <mlsouza@thoughtworks.com>
2020-12-11 12:04:58 -0300

    feat: 🎸 [DSY-1734] Adding badge component
    new badge to work as a indepedent component

---
+++ designsystem/src/main/kotlin/com/natura/android/badge/Badge.kt
@@ -0,0 +1,52 @@
+package com.natura.android.badge
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.getIntOrThrow
+import com.natura.android.R
+import com.natura.android.exceptions.MissingThemeException
+
+class Badge @JvmOverloads constructor(
+    context: Context,
+    private val attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private var badgeAttributeArray: TypedArray
+    private var tNumber: Int = 0
+    private var tShow: Int = View.INVISIBLE
+
+    val lContainer by lazy { findViewById<ConstraintLayout>(R.id.badgeContainer) }
+    val lImage by lazy { findViewById<ImageView>(R.id.badgeImage) }
+
+    init {
+        try {
+            View.inflate(context, R.layout.badge, this)
+        } catch (e: Exception) {
+            throw (MissingThemeException())
+        }
+
+        badgeAttributeArray = context.obtainStyledAttributes(attrs, R.styleable.Badge)
+
+        getAttributes()
+        configureBadge()
+
+        badgeAttributeArray.recycle()
+    }
+
+    private fun configureBadge() {
+        BadgeDrawable(context, tNumber, lImage.drawable)
+        lContainer.visibility = View.VISIBLE
+    }
+
+    private fun getAttributes() {
+        tNumber = badgeAttributeArray.getInteger(R.styleable.Badge_number, 0)
+        tShow = badgeAttributeArray.getInteger(R.styleable.Badge_android_visibility, View.INVISIBLE)
+
+    }
+}
