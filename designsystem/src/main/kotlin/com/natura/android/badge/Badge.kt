cdf13901b0ba2a7b8127ec54299a87578cdda5e9
Marcella Souza <mlsouza@thoughtworks.com>
2020-12-16 09:28:23 -0300

    docs: ✏️ [DSY-1734] Adding doc to badge
    Adding dokka dependencies to extract docs from code

--- designsystem/src/main/kotlin/com/natura/android/badge/Badge.kt
+++ designsystem/src/main/kotlin/com/natura/android/badge/Badge.kt
@@ -9,6 +9,26 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import com.natura.android.R
 import com.natura.android.exceptions.MissingThemeException
 
+/**
+ * The Badge is a screen element used to signal the user’s points of attention.
+ * Represents dynamic information such as number of notifications unread with
+ * some styles matching Nat DS appearance.
+ * This component can be used to create another component that supports a badge.
+ * Badge supports only numbers and it is possible to configure its visibility if the number shown is bigger than 0.
+ * If number is 0 the badge will not be visible.
+ * A Badge component is a [ViewGroup](https://developer.android.com/reference/android/view/ViewGroup)
+ * that renders a [BadgeDrawable].
+ * Badge is available like a GroupView at version 4.2.0 of NatDS Android.
+ *
+ * ```
+ * <com.natura.android.badge.Badge
+ *      android:id="@+id/badge"
+ *      android:layout_width="wrap_content"
+ *      android:layout_height="wrap_content"
+ *      app:badgeNumber="12" />
+ *```
+ */
+
 class Badge @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
@@ -16,19 +36,41 @@ class Badge @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
     private var badgeAttributeArray: TypedArray
     private var tNumber: Int = 0
-    private var tVisibility: Int = View.INVISIBLE
+    private var tVisibility: Int = View.VISIBLE
 
     private val lContainer by lazy { findViewById<ConstraintLayout>(R.id.badgeContainer) }
     private val lImage by lazy { findViewById<ImageView>(R.id.badgeImage) }
 
     var number: Int = 0
+        /**
+         * Specifies the number showed by badge
+         * When 0, badge is not visible, when bigger than 99, badge
+         * show 99+
+         * @return a integer with current number
+         * */
         get() = tNumber
+        /**
+         * Change the number showed by badge
+         * When 0, badge is not visible, when bigger than 99, badge
+         * show 99+
+         * @param [number] to be showed by badge
+         * */
         set(value) {
             field = value
             tNumber = value
+
         }
+
     var isVisible: Boolean = true
+        /**
+         * Specifies badge visibility.
+         * @return true if badge is visible, false if is not
+         * */
         get() = tVisibility == 0
+        /**
+         * Set badge visibility.
+         * @param [isVisible] as true to set badge as visible, false to not
+         * */
         set(value) {
             tVisibility = if(value) {
                 View.VISIBLE
@@ -55,7 +97,7 @@ class Badge @JvmOverloads constructor(
 
     private fun getAttributes() {
         tNumber = badgeAttributeArray.getInteger(R.styleable.Badge_badgeNumber, 0)
-        tVisibility = badgeAttributeArray.getInteger(R.styleable.Badge_badgeVisibility, View.INVISIBLE)
+        tVisibility = badgeAttributeArray.getInteger(R.styleable.Badge_badgeVisibility, View.VISIBLE)
 
     }
 
