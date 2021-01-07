1a10a506e02da53b15be830bf37d67cc63c58b40
Guilherme Leonel <gleonel@thoughtworks.com>
2021-01-07 16:08:35 -0300

    feat: 🎸 [DSY-1734] - Ignores Badge tests temporarily to verify execution at pipeline

--- sample/src/androidTest/kotlin/com/natura/android/sample/screenshottests/BadgeActivityScreenshotTest.kt
+++ sample/src/androidTest/kotlin/com/natura/android/sample/screenshottests/BadgeActivityScreenshotTest.kt
@@ -2,12 +2,30 @@ package com.natura.android.sample.screenshottests
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.natura.android.sample.R
 import com.natura.android.sample.components.BadgeActivity
+import org.junit.Ignore
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class BadgeActivityScreenshotTest : ScreenShotActivityTestBase() {
     @get:Rule
     var activityTestRule = ActivityTestRule(BadgeActivity::class.java, false, false)
+
+    @Ignore
+    @Test
+    fun test_Snapshot_With_Notification_Badge() {
+        val activity = activityTestRule.launchActivity(null)
+        checkScreenshot(activity, "badge_count")
+    }
+
+    @Ignore
+    @Test
+    fun test_Snapshot_After_Increment_Badge() {
+        val activity = activityTestRule.launchActivity(null)
+        performClick(R.id.incrementBadgeButton)
+        checkScreenshot(activity, "badge_count_click")
+    }
 }
