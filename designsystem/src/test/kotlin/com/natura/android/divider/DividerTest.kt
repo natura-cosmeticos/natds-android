d78029fc4cf9c89381df0910d7e5dababfe14df5
Guilherme Leonel <gleonel@thoughtworks.com>
2021-02-12 09:56:16 -0300

    feat: 🎸 [DSY-416] - Changes Divider component on SelectionControlActivity

--- designsystem/src/test/kotlin/com/natura/android/divider/DividerTest.kt
+++ designsystem/src/test/kotlin/com/natura/android/divider/DividerTest.kt
@@ -8,7 +8,6 @@ import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 
 @Ignore
 @RunWith(AndroidJUnit4::class)
