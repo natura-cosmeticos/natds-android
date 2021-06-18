d7d1d659c4d6a741f8f8cc818b00faa04d8131a8
Guilherme Leonel <gleonel@thoughtworks.com>
2021-06-18 13:57:22 -0300

    fix: become action clickable and fixes spacing at AppBar Top component

--- sample/src/main/kotlin/com/natura/android/sample/components/AppBarTopActivity.kt
+++ sample/src/main/kotlin/com/natura/android/sample/components/AppBarTopActivity.kt
@@ -2,19 +2,55 @@ package com.natura.android.sample.components
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.natura.android.iconButton.IconButton
 import com.natura.android.sample.R
 import com.natura.android.sample.setChosenDefaultWithNoActionBarTheme
-import kotlinx.android.synthetic.main.appbartop_base.*
-import kotlinx.android.synthetic.main.appbartop_button_action.*
-import kotlinx.android.synthetic.main.appbartop_search.*
-import kotlinx.android.synthetic.main.appbartop_shortcut_action.*
-import kotlinx.android.synthetic.main.appbartop_threeactions.*
-import kotlinx.android.synthetic.main.appbartop_title_center.*
-import kotlinx.android.synthetic.main.appbartop_title_long.*
-import kotlinx.android.synthetic.main.appbartop_twoactions.*
+import com.natura.android.shortcut.Shortcut
 
 class AppBarTopActivity : AppCompatActivity() {
+
+    private val appBarTopWithTwoActions by lazy { findViewById<ConstraintLayout>(R.id.appBarTopWithTwoActions) }
+    private val appBarTopWithTitleCenter by lazy { findViewById<ConstraintLayout>(R.id.appBarTopWithTitleCenter) }
+    private val appBarTopWithTitleLong by lazy { findViewById<ConstraintLayout>(R.id.appBarTopWithTitleLong) }
+    private val appBarTopWithSearch by lazy { findViewById<ConstraintLayout>(R.id.appBarTopWithSearch) }
+    private val appBarTopWithThreeActions by lazy { findViewById<ConstraintLayout>(R.id.appBarTopWithThreeActions) }
+    private val appBarTopWithShortcutAction by lazy { findViewById<ConstraintLayout>(R.id.appBarTopWithShortcutAction) }
+    private val appBarTopWithButtonAction by lazy { findViewById<ConstraintLayout>(R.id.appBarTopWithButtonAction) }
+    private val appBarTopBase by lazy { findViewById<ConstraintLayout>(R.id.appBarTopBase) }
+
+    private val baseViewFirstShorcut by lazy { findViewById<Shortcut>(R.id.baseViewFirstShorcut) }
+    private val baseViewSecondShorcut by lazy { findViewById<Shortcut>(R.id.baseViewSecondShorcut) }
+    private val baseViewThirdContained by lazy { findViewById<Shortcut>(R.id.baseViewThirdContained) }
+
+    private val buttonActionViewIconButtonActionRight by lazy { findViewById<IconButton>(R.id.buttonActionViewIconButtonActionRight) }
+    private val buttonActionViewOutlinedButtonActionRight by lazy { findViewById<Button>(R.id.buttonActionViewOutlinedButtonActionRight) }
+
+    private val searchViewIconButtonActionRight by lazy { findViewById<IconButton>(R.id.searchViewIconButtonActionRight) }
+    private val searchViewIconButtonActionLeft by lazy { findViewById<IconButton>(R.id.searchViewIconButtonActionLeft) }
+
+    private val shorcutViewFirstIconButtonActionRight by lazy { findViewById<IconButton>(R.id.shorcutViewFirstIconButtonActionRight) }
+    private val shortcutViewSecondIconButtonActionRight by lazy { findViewById<IconButton>(R.id.shortcutViewSecondIconButtonActionRight) }
+    private val shortcutViewShortcutActionLeft by lazy { findViewById<Shortcut>(R.id.shortcutViewShortcutActionLeft) }
+
+    private val threeActionsViewIconButtonActionLeft by lazy { findViewById<IconButton>(R.id.threeActionsViewIconButtonActionLeft) }
+    private val threeActionsViewFirstIconButtonActionRight by lazy { findViewById<IconButton>(R.id.threeActionsViewFirstIconButtonActionRight) }
+    private val threeActionsViewSecondIconButtonActionRight by lazy { findViewById<IconButton>(R.id.threeActionsViewSecondIconButtonActionRight) }
+    private val threeActionsViewThirdIconButtonActionRight by lazy { findViewById<IconButton>(R.id.threeActionsViewThirdIconButtonActionRight) }
+
+    private val titleCenterViewIconButtonActionLeft by lazy { findViewById<IconButton>(R.id.titleCenterViewIconButtonActionLeft) }
+    private val titleCenterViewIconButtonActionRight by lazy { findViewById<IconButton>(R.id.titleCenterViewIconButtonActionRight) }
+
+    private val titleLongViewIconButtonActionLeft by lazy { findViewById<IconButton>(R.id.titleLongViewIconButtonActionLeft) }
+    private val titleLongViewFirstIconButtonActionRight by lazy { findViewById<IconButton>(R.id.titleLongViewFirstIconButtonActionRight) }
+    private val titleLongViewSecondIconButtonActionRight by lazy { findViewById<IconButton>(R.id.titleLongViewSecondIconButtonActionRight) }
+    private val titleLongViewThirdIconButtonActionRight by lazy { findViewById<IconButton>(R.id.titleLongViewThirdIconButtonActionRight) }
+
+    private val twoActionsViewFirstIconButtonActionRight by lazy { findViewById<IconButton>(R.id.twoActionsViewFirstIconButtonActionRight) }
+    private val twoActionsViewSecondIconButtonActionRight by lazy { findViewById<IconButton>(R.id.twoActionsViewSecondIconButtonActionRight) }
+    private val twoActionsViewIconButtonActionLeft by lazy { findViewById<IconButton>(R.id.twoActionsViewIconButtonActionLeft) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setChosenDefaultWithNoActionBarTheme()
@@ -24,6 +60,8 @@ class AppBarTopActivity : AppCompatActivity() {
         val pattern = intent.getIntExtra("pattern", PATTERN_APPBARTOP_TWOACTIONS)
 
         setContentView(R.layout.activity_appbar_top)
+
+        createListeners()
 
         when (pattern) {
             PATTERN_APPBARTOP_TWOACTIONS -> appBarTopWithTwoActions.visibility = View.VISIBLE
@@ -35,6 +73,40 @@ class AppBarTopActivity : AppCompatActivity() {
             PATTERN_APPBARTOP_BUTTON -> appBarTopWithButtonAction.visibility = View.VISIBLE
             PATTERN_APPBARTOP_BASE -> appBarTopBase.visibility = View.VISIBLE
         }
+    }
+
+    private fun createListeners() {
+        baseViewFirstShorcut.setOnClickListener {}
+        baseViewSecondShorcut.setOnClickListener {}
+        baseViewThirdContained.setOnClickListener {}
+
+        buttonActionViewIconButtonActionRight.setOnClickListener {}
+        buttonActionViewOutlinedButtonActionRight.setOnClickListener {}
+
+        searchViewIconButtonActionRight.setOnClickListener {}
+        searchViewIconButtonActionLeft.setOnClickListener {}
+
+        shorcutViewFirstIconButtonActionRight.setOnClickListener {}
+        shortcutViewSecondIconButtonActionRight.setOnClickListener {}
+        shortcutViewShortcutActionLeft.setOnClickListener {}
+
+        threeActionsViewIconButtonActionLeft.setOnClickListener {}
+        threeActionsViewFirstIconButtonActionRight.setOnClickListener {}
+        threeActionsViewSecondIconButtonActionRight.setOnClickListener {}
+        threeActionsViewThirdIconButtonActionRight.setOnClickListener {}
+
+        titleCenterViewIconButtonActionLeft.setOnClickListener {}
+        titleCenterViewIconButtonActionRight.setOnClickListener {}
+
+        titleLongViewIconButtonActionLeft.setOnClickListener {}
+        titleLongViewFirstIconButtonActionRight.setOnClickListener {}
+        titleLongViewSecondIconButtonActionRight.setOnClickListener {}
+        titleLongViewThirdIconButtonActionRight.setOnClickListener {}
+
+        twoActionsViewFirstIconButtonActionRight.setOnClickListener {}
+        twoActionsViewSecondIconButtonActionRight.setOnClickListener {}
+
+        twoActionsViewIconButtonActionLeft.setOnClickListener {}
     }
 
     companion object {
