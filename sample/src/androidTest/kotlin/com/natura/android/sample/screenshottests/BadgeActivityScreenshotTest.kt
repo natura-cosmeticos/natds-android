9e9f80c746fb91ddce7c9513be5f7903a0a9bf9e
Guilherme Leonel <gleonel@thoughtworks.com>
2021-08-17 17:14:58 -0300

    chore: fixes Lint

--- sample/src/androidTest/kotlin/com/natura/android/sample/screenshottests/BadgeActivityScreenshotTest.kt
+++ sample/src/androidTest/kotlin/com/natura/android/sample/screenshottests/BadgeActivityScreenshotTest.kt
@@ -2,10 +2,8 @@ package com.natura.android.sample.screenshottests
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import com.natura.android.sample.R
 import com.natura.android.sample.components.BadgeActivity
 import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
