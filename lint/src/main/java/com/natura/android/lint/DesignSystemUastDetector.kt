ec19a37e5725abe26fcaacca5427153956ae4b2a
Daniel Nazareth <daniel.nazareth@thoughtworks.com>
2021-09-13 16:10:23 -0300

    STEM-2147: add lint rules for DS

---
+++ lint/src/main/java/com/natura/android/lint/DesignSystemUastDetector.kt
@@ -0,0 +1,58 @@
+package com.natura.android.lint
+
+import com.android.tools.lint.detector.api.Scope.JAVA_FILE
+import com.android.tools.lint.detector.api.Category.Companion.CORRECTNESS
+import org.jetbrains.uast.UClass
+import org.jetbrains.uast.UElement
+import org.jetbrains.uast.UVariable
+import com.android.tools.lint.client.api.UElementHandler
+import com.android.tools.lint.detector.api.*
+import com.android.tools.lint.detector.api.LintFix.*
+import com.intellij.psi.PsiType
+import com.natura.android.lint.IssueRegistry.Companion.ISSUE_NON_DIALOG_STANDARD_VIEW
+import com.natura.android.lint.IssueRegistry.Companion.ISSUE_NON_DS_VIEW
+
+class DesignSystemUastDetector : Detector(), Detector.UastScanner {
+
+    companion object {
+        private const val ALERT_DIALOG_VIEW = "android.app.AlertDialog"
+
+        private const val ANDROIDX_ALERT_DIALOG_VIEW = "androidx.appcompat.app.AlertDialog"
+
+        private const val NAT_DS_DIALOG_STANDARD_VIEW = "com.natura.android.dialog.DialogStandard"
+    }
+
+    override fun getApplicableUastTypes() =
+        listOf<Class<out UElement>>(UVariable::class.java, UClass::class.java)
+
+    override fun createUastHandler(context: JavaContext) = UastHandler(context)
+
+    inner class UastHandler(private val context: JavaContext) : UElementHandler() {
+
+        override fun visitVariable(node: UVariable) = process(node.type, node)
+
+        override fun visitClass(node: UClass) = node.uastSuperTypes.forEach { process(it.type, it) }
+
+        private fun process(type: PsiType, node: UElement) {
+            val qualifiedName = context.evaluator.getTypeClass(type)?.getQualifiedName()
+            val issue = getSuggestedIssue(qualifiedName)
+
+            if (issue == null) {
+                return
+            }
+
+            context.report(
+                issue = issue,
+                scope = node,
+                location = context.getLocation(node),
+                message = issue.getExplanation(TextFormat.TEXT)
+            )
+        }
+
+        private fun getSuggestedIssue(name: String?) = when (name) {
+            ANDROIDX_ALERT_DIALOG_VIEW, ALERT_DIALOG_VIEW -> ISSUE_NON_DIALOG_STANDARD_VIEW
+            else -> null
+        }
+    }
+
+}
