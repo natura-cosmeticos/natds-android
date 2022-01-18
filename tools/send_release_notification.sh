78c03fc42d59ab7f0f6e54e7caa323b789d535e8
Guilherme Leonel <gleonel@thoughtworks.com>
2022-01-18 09:51:28 -0300

    fix: fixes shell warnings

--- tools/send_release_notification.sh
+++ tools/send_release_notification.sh
@@ -5,7 +5,7 @@ cp ./tools/message_teams.json ./tools/updated_message_teams.json
 
 # get and update version at message
 NEW_VERSION=$(cat ./version.txt)
-sed -i '' 's/VERSIONVALUE/'$NEW_VERSION'/g' ./tools/updated_message_teams.json
+sed -i '' 's/VERSIONVALUE/'"$NEW_VERSION"'/g' ./tools/updated_message_teams.json
 
 releaselinecontent='https://github.com/natura-cosmeticos/natds-android/compare/'
 
@@ -15,7 +15,7 @@ patch=$(echo "$NEW_VERSION" | cut -d. -f3)
 rgxversion="v.$major\.$minor\.$patch" # regex for new version
 
 # save last version output to file
-awk 's{print $0 > "./tools/message_release.txt"}; $0~v {print $0 > "./tools/message_release.txt"; s=1; next}; (!($0~v) && $0~l) {s=0};' RS= v=$rgxversion l=$releaselinecontent CHANGELOG.md
+awk 's{print $0 > "./tools/message_release.txt"}; $0~v {print $0 > "./tools/message_release.txt"; s=1; next}; (!($0~v) && $0~l) {s=0};' RS= v="$rgxversion" l=$releaselinecontent CHANGELOG.md
 # erase last line of file
 sed -i '' -e '$ d' ./tools/message_release.txt
 
@@ -23,11 +23,11 @@ sed -i '' -e '$ d' ./tools/message_release.txt
 awk '{printf "%s\\\n ",$0 > "./tools/message_text.txt"} END {print ""}' ./tools/message_release.txt
 
 # update release message at json
-CHANGELOG_UPDATES=`cat ./tools/message_text.txt`
+CHANGELOG_UPDATES=$(cat ./tools/message_text.txt)
 sed -i '' "s|CHANGELOGMESSAGE|$CHANGELOG_UPDATES|g" ./tools/updated_message_teams.json
 
 # send message
-message=`cat ./tools/updated_message_teams.json`
+message=$(cat ./tools/updated_message_teams.json)
 curl -H 'Content-Type: application/json' -d "$message" "$TEAMS_RELEASE_WEBHOOK"
 # remove helper files
 rm ./tools/updated_message_teams.json
