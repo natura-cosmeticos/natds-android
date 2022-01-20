39c612417368c804618b806b09a1bf2c732110a5
Guilherme Leonel <gleonel@thoughtworks.com>
2022-01-20 15:45:51 -0300

    refactor: removes unused Travis script file

--- tools/pipeline_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# task :sample:verifyDebugAndroidTestScreenshotTest removed because of an instability in Travis emulator
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    ./gradlew clean build publish -PdisablePreDex --stacktrace
-else
-    ./gradlew clean build -PdisablePreDex --stacktrace
-fi
