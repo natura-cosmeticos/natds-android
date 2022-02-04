02f3cac07d527eb756a5a8b16a81770541498be1
Guilherme Leonel <gleonel@thoughtworks.com>
2022-02-04 08:51:57 -0300

    chore: adds news screenshots and removes ignore annotation from some tests

--- sample/src/androidTest/kotlin/com/natura/android/sample/components/DialogActivityFunctionalTest.kt
+++ sample/src/androidTest/kotlin/com/natura/android/sample/components/DialogActivityFunctionalTest.kt
@@ -12,11 +12,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.natura.android.sample.R
 import org.hamcrest.CoreMatchers.not
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@Ignore
 @RunWith(AndroidJUnit4::class)
 class DialogActivityFunctionalTest {
     private lateinit var dialogActivityScenario: ActivityScenario<DialogActivity>
@@ -40,7 +38,7 @@ class DialogActivityFunctionalTest {
     fun shouldShowDialogCustomContent() {
         onView(withId(R.id.standardDialogButton)).perform(click())
 
-        onView(withId(R.id.dialogCustomContent))
+        onView(withText("Long text that should be substitied for some dialog text. This might actually take two lines or more"))
             .inRoot(isDialog())
             .check(matches(isDisplayed()))
     }
