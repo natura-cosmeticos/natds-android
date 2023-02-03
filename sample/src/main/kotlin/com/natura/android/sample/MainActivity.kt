417a4b5abe0918795fc79091f72b4fb87e6b6717
Design-System-Natura <65979129+Design-System-Natura@users.noreply.github.com>
2023-02-03 11:51:03 -0300

    fix: build fix

--- sample/src/main/kotlin/com/natura/android/sample/MainActivity.kt
+++ sample/src/main/kotlin/com/natura/android/sample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.natura.android.sample
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.natura.android.sample.components.*
 import com.natura.android.sample.patterns.ErrorActivity
 import com.natura.android.sample.components.listitem.ListItemActivity
 import com.natura.android.sample.databinding.ActivityMainBinding
@@ -75,6 +76,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.dialogButton.setOnClickListener {
             startActivity(Intent(this, DialogActivity::class.java))
+
         }
 
         binding.btnExpansionPanel.setOnClickListener {
