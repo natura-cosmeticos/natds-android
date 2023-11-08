42fa49c4e436ee7335971eae828de169d49c0c0b
Andrei Benincasa <andrei.b.santos@accenture.com>
2023-11-08 14:44:19 -0300

    Adding remaining variants.

--- designsystem/src/main/kotlin/com/natura/android/rating/Rating.kt
+++ designsystem/src/main/kotlin/com/natura/android/rating/Rating.kt
@@ -11,8 +11,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.AbstractComposeView
@@ -28,33 +28,83 @@ class Rating @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AbstractComposeView(context, attrs, defStyleAttr) {
 
-    @Composable
-    override fun Content() {
-        Column {
-            Row {
-                var rating by remember { mutableStateOf(0) }
-                for (i in 1..MAX_RATING) {
-                    IconButton(onClick = { rating = i }) {
-                        if (i <= rating) {
-                            Star(isFilled = true)
-                        } else {
-                            Star(isFilled = false)
-                        }
-                    }
-                }
+    var rate by mutableStateOf(0)
+        private set
+    var variant by mutableStateOf(Variant.INPUT)
+        private set
+    var hint by mutableStateOf(String())
+        private set
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.Rating,
+            0, 0
+        ).apply {
+            try {
+                rate = getInt(R.styleable.Rating_rate, 0)
+                variant = Variant.values()[getInt(R.styleable.Rating_variant, 0)]
+                hint = getString(R.styleable.Rating_hint) ?: String()
+            } finally {
+                recycle()
             }
-            Text(
-                text = "Placeholder",
-                style = natdsTypography.caption,
-                color = getThemeColorForCompose(attrColorId = R.attr.colorMediumEmphasis)
-            )
         }
     }
 
     @Composable
-    fun Star(isFilled: Boolean) {
+    override fun Content() {
+        when (variant) {
+            Variant.INPUT -> InputRating()
+            Variant.READ_ONLY -> ReadOnlyRating()
+            Variant.COUNTER -> CounterRating()
+        }
+    }
+
+    @Composable
+    fun InputRating() {
+        Column {
+            Row {
+                for (i in 1..MAX_RATE) {
+                    IconButton(onClick = { rate = i }) {
+                        if (i <= rate) {
+                            Star(isFilled = true, isEnabled = true)
+                        } else {
+                            Star(isFilled = false, isEnabled = false)
+                        }
+                    }
+                }
+            }
+            if (hint.isNotEmpty()) Hint(hint = hint)
+        }
+    }
+
+    @Composable
+    fun ReadOnlyRating() {
+        Column {
+            Row {
+                for (i in 1..MAX_RATE) {
+                    if (i <= rate) {
+                        Star(isFilled = true, isEnabled = true)
+                    } else {
+                        Star(isFilled = true, isEnabled = false)
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun CounterRating() {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Star(isFilled = true, isEnabled = true)
+            if (hint.isNotEmpty()) Hint(hint = hint)
+        }
+    }
+
+    @Composable
+    fun Star(isFilled: Boolean, isEnabled: Boolean) {
         val iconName = if (isFilled) FILLED_STAR_ICON_NAME else OUTLINED_STAR_ICON_NAME
-        val attrColorId = if (isFilled) R.attr.colorPrimary else R.attr.colorMediumEmphasis
+        val attrColorId = if (isEnabled) R.attr.colorPrimary else R.attr.colorMediumEmphasis
 
         Icon(
             imageVector = ImageVector.vectorResource(
@@ -66,8 +116,23 @@ class Rating @JvmOverloads constructor(
         )
     }
 
+    @Composable
+    fun Hint(hint: String) {
+        Text(
+            text = hint,
+            style = natdsTypography.caption,
+            color = getThemeColorForCompose(attrColorId = R.attr.colorMediumEmphasis)
+        )
+    }
+
+    enum class Variant {
+        INPUT,
+        READ_ONLY,
+        COUNTER
+    }
+
     companion object {
-        const val MAX_RATING = 5
+        const val MAX_RATE = 5
         const val FILLED_STAR_ICON_NAME = "filled_action_rating"
         const val OUTLINED_STAR_ICON_NAME = "outlined_action_rating"
     }
