5b8acb12bb0edcbc3d3078cd3a45564e8a31055c
hayna-foursys <131673198+hayna-foursys@users.noreply.github.com>
2024-03-18 11:29:44 -0300

    breaking: change bump version script

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git fetch
 
 # Pega a última tag
 LAST_TAG=$(git describe --tags --abbrev=0)
@@ -7,24 +8,31 @@ LAST_TAG=$(git describe --tags --abbrev=0)
 if git log "${LAST_TAG}..HEAD" --format='%s' | grep -q -E 'major:'; then
     # Se "major:" for encontrado, aumenta a versão major
     npx standard-version --release-as major
-# Verifica se existe algum commit com 'breaking:' desde a última tag
-elif git log "${LAST_TAG}..HEAD" --format='%b' | grep -q -E 'breaking:'; then
+# Verifica se existe algum commit com 'break:' desde a última tag
+elif git log "${LAST_TAG}..HEAD" --format='%b' | grep -q -E '^breaking:'; then
     # Se "breaking:" for encontrado, aumenta a versão major
     npx standard-version --release-as major
-else
-    # Se nenhum dos anteriores for encontrado, segue o procedimento padrão para determinar o tipo de release
+# Verifica por outros tipos de commits
+elif git log "${LAST_TAG}..HEAD" --format='%s' | grep -q -i -E '^feat|fix|perf|refactor|revert$'; then
+    # Procede com a versão padrão
     npx standard-version
+else
+    echo "No applicable changes since the previous tag, skipping..."
 fi
 
 # Pega a versão atual do arquivo version.txt
 NATDS_VERSION=$(cat ./version.txt)
 
-# Atualiza a versão no agvtool e nos projetos
+# Adiciona a versão como variável de ambiente para usos posteriores
 envman add --key NATDS_VERSION --value "$NATDS_VERSION"
+
+# Limpa arquivos não rastreados e diretórios
 git clean -f -d
 
-# Faz um commit com as alterações
-git commit -m "chore: updates version"
-
-# Empurra os commits e as tags
-git push --follow-tags origin HEAD
+# Commit as mudanças caso tenham ocorrido
+if [ -n "$(git status --porcelain)" ]; then
+  git commit -am "chore: Updates version"
+  git push --follow-tags origin HEAD
+else
+  echo "No files to commit."
+fi
