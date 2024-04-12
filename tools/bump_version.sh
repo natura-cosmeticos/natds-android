8f9b61d93a65f99dec1a08d0937841f3cfce4542
hayna-foursys <hayna.cardoso@foursys.com.br>
2024-04-12 17:44:14 -0300

    Update bump_version.sh

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 git fetch
- if \
-     { git log "$( git describe --tags --abbrev=0 )..HEAD" --format='%s' | cut -d: -f1 | sort -u | sed -e 's/([^)]*)//' | grep -q -i -E '^feat|fix|perf|refactor|revert$' ; } || \
-     { git log "$( git describe --tags --abbrev=0 )..HEAD" --format='%s' | cut -d: -f1 | sort -u | sed -e 's/([^)]*)//' | grep -q -E '\!$' ; } || \
-     { git log "$( git describe --tags --abbrev=0 )..HEAD" --format='%b' | grep -q -E '^break:' ; }
- then
-     npx standard-version
-     NATDS_VERSION=$(cat ./version.txt)
-    envman add --key NATDS_VERSION --value "$NATDS_VERSION"
-    git clean -f -d
-    git commit -m "chore: Updates version"
+
+# Pega a última tag
+LAST_TAG=$(git describe --tags --abbrev=0)
+
+# Verifica se existe algum commit com 'major:' no início desde a última tag
+if git log "${LAST_TAG}..HEAD" --format='%s' | grep -q -E '^major:'; then
+    # Se "major:" for encontrado no início, aumenta a versão major
+    npx standard-version --release-as major
+
+# Verifica se existe algum commit com 'breaking:' no início desde a última tag
+elif git log "${LAST_TAG}..HEAD" --format='%s' | grep -q -E '^breaking:'; then
+    # Se "breaking:" for encontrado no início, aumenta a versão minor
+    npx standard-version --release-as minor
+
+else
+    # Para todos os outros casos, segue o procedimento padrão que normalmente aumenta o patch
+    npx standard-version
+fi
+
+# Pega a versão atual do arquivo version.txt
+NATDS_VERSION=$(cat ./version.txt)
+
+# Adiciona a versão como variável de ambiente para usos posteriores
+envman add --key NATDS_VERSION --value "$NATDS_VERSION"
+
+# Limpa arquivos não rastreados e diretórios
+git clean -f -d
+
+# Verifica se existem arquivos para commitar e faz o commit se necessário
+if [ -n "$(git status --porcelain)" ]; then
+    git commit -am "chore: Updates version"
     git push --follow-tags origin HEAD
 else
-    echo "No applicable changes since the previous tag, skipping..."
+    echo "No files to commit."
 fi
