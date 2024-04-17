1ab3ec59c99f0f75170534f2a74269677c51887b
hayna-foursys <hayna.cardoso@foursys.com.br>
2024-04-17 13:33:07 -0300

    fixing script to allow major minor and patch

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 git fetch
 
-# Pega a última tag
 LAST_TAG=$(git describe --tags --abbrev=0)
 
-# Verifica se existe algum commit com 'major:' no início desde a última tag
+RELEASE_TYPE="patch"
+
 if git log "${LAST_TAG}..HEAD" --format='%s' | grep -q -E '^major:'; then
-    # Se "major:" for encontrado no início, aumenta a versão major
-    npx standard-version --release-as major
-
-# Verifica se existe algum commit com 'breaking:' no início desde a última tag
+    RELEASE_TYPE="major"
 elif git log "${LAST_TAG}..HEAD" --format='%s' | grep -q -E '^breaking:'; then
-    # Se "breaking:" for encontrado no início, aumenta a versão minor
-    npx standard-version --release-as minor
-
-else
-    # Para todos os outros casos, segue o procedimento padrão que normalmente aumenta o patch
-    npx standard-version
+    RELEASE_TYPE="minor"
+elif git log "${LAST_TAG}..HEAD" --format='%s' | grep -q -i -E '^fixing|^adding|^bump version|feat|fix|perf|refactor|revert'; then
+    RELEASE_TYPE="patch"
 fi
 
-# Pega a versão atual do arquivo version.txt
+npx standard-version --release-as $RELEASE_TYPE
+
 NATDS_VERSION=$(cat ./version.txt)
 
-# Adiciona a versão como variável de ambiente para usos posteriores
 envman add --key NATDS_VERSION --value "$NATDS_VERSION"
 
-# Limpa arquivos não rastreados e diretórios
 git clean -f -d
 
-# Verifica se existem arquivos para commitar e faz o commit se necessário
 if [ -n "$(git status --porcelain)" ]; then
     git commit -am "chore: Updates version"
     git push --follow-tags origin HEAD
